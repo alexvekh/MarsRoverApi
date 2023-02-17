7271a827be64dc95feb6a10e38962f06a889c404
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-02-16 22:38:45 -0500

    Unit Test RestTemplate

--- src/main/java/com/boocrun/HomeController.java
+++ src/main/java/com/boocrun/HomeController.java
@@ -1,15 +1,16 @@
 package com.boocrun;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
   @GetMapping("/")
-  public String getHomeView () {
+  public String getHomeView (ModelMap model) {
+    
+
     return "index";
   }
-  
-  
-
 }

7414c582cb168d557139306862ec142bdbdea0b6
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-06-01 23:59:34 -0400

    work in process

--- src/main/java/com/boocrun/web/HomeController.java
+++ src/main/java/com/boocrun/web/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.boocrun.response.MarsRoverApiResponse;
 import com.boocrun.service.MarsRoverApiService;
@@ -16,6 +18,13 @@ public class HomeController {
   @GetMapping("/")
   public String getHomeView (ModelMap model) {
     MarsRoverApiResponse roverData = roverService.getRoverData("curiosity");  //opportunity
+    model.put("roverData", roverData);
+    return "index";
+  }
+  
+  @PostMapping("/")
+  public String postHomeView (ModelMap model, @RequestParam String marsApiRoverData) {
+    MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);  //opportunity
     model.put("roverData", roverData);
     return "index";
   }
